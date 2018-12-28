71f59656fc24aef7c9950813bd79aca87ed26c82
makutak <takkouno@gmail.com>
2018-12-28 21:09:21 +0900

    fix

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject infix-calculator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure infix calculator"
+  :url "https://github.com/makutak/infix-calculator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
